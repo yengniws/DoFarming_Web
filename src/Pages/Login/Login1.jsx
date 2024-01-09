a60570c4b01849e691f8c2886c768da559c3add2
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-01-09 17:04:01 +0900

    Login 1, 3p development completed

--- src/Pages/Login/Login1.jsx
+++ src/Pages/Login/Login1.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "../../styles/login/Login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login1 = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/login2");
+  };
+
   return (
-    <div>
-      <p>Login1</p>
+    <div className="login1 wrap">
+      <div className="container">
+        <div className="maintxt">
+          <p className="txt1">
+            우리에게 주어진<br></br>공평한 하루
+          </p>
+          <p className="txt2">
+            남에게 있는 것을 바라보는 것이 아닌,<br></br> 나에게 주어진 것에
+            감사하는 마음
+          </p>
+        </div>
+        <button id="login1_submit_btn" onClick={handleButtonClick}>
+          <strong>반가워👋</strong>
+        </button>
+      </div>
     </div>
   );
 };
--- src/Pages/Login/Login3.jsx
+++ src/Pages/Login/Login3.jsx
@@ -1,11 +1,118 @@
 import "../../styles/login/Login.css";
+import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
 
-const Login3 = () => {
+function Login3() {
+  const navigate = useNavigate();
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const toggleOption = (option) => {
+    const updatedOptions = [...selectedOptions];
+
+    if (updatedOptions.includes(option)) {
+      updatedOptions.splice(updatedOptions.indexOf(option), 1);
+    } else {
+      updatedOptions.push(option);
+    }
+
+    setSelectedOptions(updatedOptions);
+  };
+
+  const handleButtonClick = () => {
+    const selectedOptionsString = selectedOptions
+      .map(encodeURIComponent)
+      .join(",");
+    navigate("/Routine", { state: { userData: selectedOptionsString } });
+  };
+
+  const BtnDisabled = () => {
+    return selectedOptions.length === 0;
+  };
+
   return (
-    <div>
-      <p>Login3</p>
+    <div className="container">
+      <p className="Login3_txt1">
+        <strong>어떤 고민</strong>이 있으세요?
+      </p>
+      <div className="options" id="options">
+        <div
+          className={`option ${
+            selectedOptions.includes("학업") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("학업")}
+        >
+          학업
+        </div>
+        <div
+          className={`option ${
+            selectedOptions.includes("취업") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("취업")}
+        >
+          취업
+        </div>
+        <div
+          className={`option ${
+            selectedOptions.includes("직장생활") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("직장생활")}
+        >
+          직장생활
+        </div>
+        <div
+          className={`option ${
+            selectedOptions.includes("인간관계") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("인간관계")}
+        >
+          인간관계
+        </div>
+        <div
+          className={`option ${
+            selectedOptions.includes("사랑") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("사랑")}
+        >
+          사랑
+        </div>
+        <div
+          className={`option ${
+            selectedOptions.includes("스트레스") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("스트레스")}
+        >
+          스트레스
+        </div>
+        <div
+          className={`option ${
+            selectedOptions.includes("미래에 대한 불안감") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("미래에 대한 불안감")}
+        >
+          미래에 대한
+          <br />
+          불안감
+        </div>
+        <div
+          className={`option ${
+            selectedOptions.includes("건강") ? "selected" : ""
+          }`}
+          onClick={() => toggleOption("건강")}
+        >
+          건강
+        </div>
+      </div>
+      <Link to="/Login4">
+        <button
+          id="SelectclearBtn"
+          onClick={handleButtonClick}
+          disabled={BtnDisabled()}
+        >
+          선택 완료
+        </button>
+      </Link>
     </div>
   );
-};
+}
 
 export default Login3;
